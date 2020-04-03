792a10741d6730c1f468f8558ee2d15fb1d1d212
beatap123 <krystianpavlov@gmail.com>
2020-04-03 10:43:36 +0200

    zmiany w settings

--- mojprojekt/settings.py
+++ mojprojekt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6&8^7j6ufr!c=h@c5bo07he$jk%0mm0@dsj61h(j)ks8cb84nw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['beatap.pythonanywhere.com']
+ALLOWED_HOSTS = ['beatap.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,4 +118,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/firmy/mojprojekt/firma/static/'
